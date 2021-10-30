15bf77301ebd17e354b82bfc2b6ce7bb2e86c012
WandaSzmuc <wandszmuc@gmail.com>
2021-10-30 15:47:18 +0200

    contact component and styles extended

--- src/components/HomeContact.js
+++ src/components/HomeContact.js
@@ -1,5 +1,31 @@
 import React from "react";
-
+import decorator from "../assets/Decoration.svg";
 export default function HomeContact() {
-  return <div></div>;
+  return (
+    <div className="main_contact--container">
+      <div className="main_contact--section">
+        <div className="main_contact--decoration"></div>{" "}
+      </div>
+
+      <div className="main_contact--section main_contact--form">
+        <h1>Skontaktuj się z nami</h1>
+        <img src={decorator} />
+        <form className="contact_form">
+          <div className="contact_form--section">
+            <div className="contact_form--container">
+              <label>Wpisz swoje imię</label>
+              <input></input>
+            </div>
+            <div className="contact_form--container">
+              <label>Wpisz swój email</label>
+              <input></input>
+            </div>
+          </div>
+          <label>Wpisz swoją wiadomość</label>
+          <input className="message_input"></input>
+          <button>Wyślij</button>
+        </form>
+      </div>
+    </div>
+  );
 }
