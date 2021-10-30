6e4b60619bc630d04f7b91d4a8ed04f437aba8cf
WandaSzmuc <wandszmuc@gmail.com>
2021-10-30 13:01:57 +0200

    extended component foursteps and its styles

--- src/components/HomeFourSteps.js
+++ src/components/HomeFourSteps.js
@@ -1,5 +1,48 @@
 import React from "react";
+import decorator from "../assets/Decoration.svg";
+import icon1 from "../assets/Icon-1.svg";
+import icon2 from "../assets/Icon-2.svg";
+import icon3 from "../assets/Icon-3.svg";
+import icon4 from "../assets/Icon-4.svg";
 
 export default function HomeFourSteps() {
-  return <div></div>;
+  return (
+    <div className="four_steps--container">
+      <div className="four_steps--headline_container">
+        <h1 className="four_steps--headline">Wystarczą 4 proste kroki</h1>
+        <img src={decorator} className="four_steps--decorator" />
+      </div>
+      <div className="four_steps--steps">
+        <div className="four_steps--step">
+          <img src={icon1} />
+          <h1>Wybierz rzeczy</h1>
+          <div></div>
+          <p>ubrania, zabawki, sprzęt i inne</p>
+        </div>
+        <div className="four_steps--step">
+          <img src={icon2} />
+          <h1>Spakuj je</h1>
+          <div></div>
+          <p> skorzystaj z worków na śmieci</p>
+        </div>
+        <div className="four_steps--step">
+          <img src={icon3} />
+          <h1>Zdecyduj komu chcesz pomóc</h1>
+          <div></div>
+          <p>wybierz zaufane miejsce</p>
+        </div>
+        <div className="four_steps--step">
+          <img src={icon4} />
+          <h1>Zamów kuriera</h1>
+          <div></div>
+          <p>kurier przyjedzie w dogodnym terminie</p>
+        </div>
+      </div>
+      <div className="four_steps--button_container">
+        <a href="#" className="four_steps--button">
+          Oddaj rzeczy
+        </a>
+      </div>
+    </div>
+  );
 }
--- src/components/HomeThreeColumns.js
+++ src/components/HomeThreeColumns.js
@@ -2,33 +2,35 @@ import React from "react";
 
 export default function HomeThreeColumns() {
   return (
-    <div className="three_columns--section">
-      <div className="column_container">
-        <h1 className="three_columns--el">10</h1>
-        <h2 className="three_columns--el">oddanych worków</h2>
-        <p className="three_columns--el">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
-          deleniti nesciunt consectetur? Unde, illum asperiores, nulla eos et
-          itaque.
-        </p>
-      </div>
-      <div className="column_container">
-        <h1 className="three_columns--el">5</h1>
-        <h2 className="three_columns--el">wspartych organizacji</h2>
-        <p className="three_columns--el">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
-          deleniti nesciunt consectetur? Unde, illum asperiores, nulla eos et
-          itaque.
-        </p>
-      </div>
-      <div className="column_container">
-        <h1 className="three_columns--el">7</h1>
-        <h2 className="three_columns--el">zorganizowanych zbiórek</h2>
-        <p className="three_columns--el">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
-          deleniti nesciunt consectetur? Unde, illum asperiores, nulla eos et
-          itaque.
-        </p>
+    <div className="three_columns">
+      <div className="three_columns--section">
+        <div className="column_container">
+          <h1 className="three_columns--el">10</h1>
+          <h2 className="three_columns--el">oddanych worków</h2>
+          <p className="three_columns--el">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
+            deleniti nesciunt consectetur? Unde, illum asperiores, nulla eos et
+            itaque.
+          </p>
+        </div>
+        <div className="column_container">
+          <h1 className="three_columns--el">5</h1>
+          <h2 className="three_columns--el">wspartych organizacji</h2>
+          <p className="three_columns--el">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
+            deleniti nesciunt consectetur? Unde, illum asperiores, nulla eos et
+            itaque.
+          </p>
+        </div>
+        <div className="column_container">
+          <h1 className="three_columns--el">7</h1>
+          <h2 className="three_columns--el">zorganizowanych zbiórek</h2>
+          <p className="three_columns--el">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
+            deleniti nesciunt consectetur? Unde, illum asperiores, nulla eos et
+            itaque.
+          </p>
+        </div>
       </div>
     </div>
   );
