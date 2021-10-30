39b27e2ce4a6f6a4f7d908232dbcffcbe3624c1d
WandaSzmuc <wandszmuc@gmail.com>
2021-10-30 11:38:50 +0200

    extended three columns componend and its styles

--- src/components/HomeThreeColumns.js
+++ src/components/HomeThreeColumns.js
@@ -1,5 +1,35 @@
 import React from "react";
 
 export default function HomeThreeColumns() {
-  return <div></div>;
+  return (
+    <div className="three_columns--section">
+      <div className="column_container">
+        <h1 className="three_columns--el">10</h1>
+        <h2 className="three_columns--el">oddanych worków</h2>
+        <p className="three_columns--el">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
+          deleniti nesciunt consectetur? Unde, illum asperiores, nulla eos et
+          itaque.
+        </p>
+      </div>
+      <div className="column_container">
+        <h1 className="three_columns--el">5</h1>
+        <h2 className="three_columns--el">wspartych organizacji</h2>
+        <p className="three_columns--el">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
+          deleniti nesciunt consectetur? Unde, illum asperiores, nulla eos et
+          itaque.
+        </p>
+      </div>
+      <div className="column_container">
+        <h1 className="three_columns--el">7</h1>
+        <h2 className="three_columns--el">zorganizowanych zbiórek</h2>
+        <p className="three_columns--el">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
+          deleniti nesciunt consectetur? Unde, illum asperiores, nulla eos et
+          itaque.
+        </p>
+      </div>
+    </div>
+  );
 }
