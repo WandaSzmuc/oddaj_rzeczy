b058115becec444437bb4d48b61f5c0e64d7c18a
WandaSzmuc <wandszmuc@gmail.com>
2021-10-30 14:11:49 +0200

    about us component with styles extended

--- src/components/HomeAboutUs.js
+++ src/components/HomeAboutUs.js
@@ -1,5 +1,22 @@
 import React from "react";
+import decorator from "../assets/Decoration.svg";
+import signature from "../assets/Signature.svg";
 
 export default function HomeAboutUs() {
-  return <div></div>;
+  return (
+    <div className="about_us--container">
+      <div className="about_us--section about_us--description">
+        <h1>O nas</h1>
+        <img src={decorator} />
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+        <img src={signature} className="about_us--signature" />
+      </div>
+      <div className="about_us--section about_us--img">/></div>
+    </div>
+  );
 }
